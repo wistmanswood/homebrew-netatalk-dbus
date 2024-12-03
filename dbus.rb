f5fe7c41ca1a2dbf78fe035cd4a36136db0cce9a
dgsga <dgsga@icloud.com>
2024-12-03 14:02:39 +0000

    Test

--- dbus.rb
+++ dbus.rb
@@ -2,8 +2,8 @@ class Dbus < Formula
   # releases: even (1.12.x) = stable, odd (1.13.x) = development
   desc "Message bus system, providing inter-application communication"
   homepage "https://wiki.freedesktop.org/www/Software/dbus"
-  url "https://dbus.freedesktop.org/releases/dbus/dbus-1.15.8.tar.xz"
-  sha256 "84fc597e6ec82f05dc18a7d12c17046f95bad7be99fc03c15bc254c4701ed204"
+  url "https://dbus.freedesktop.org/releases/dbus/dbus-1.15.12.tar.xz"
+  sha256 "0589c9c707dd593e31f0709caefa5828e69c668c887a7c0d2e5ba445a86bae4d"
   license any_of: ["AFL-2.1", "GPL-2.0-or-later"]
 
   livecheck do
@@ -11,39 +11,48 @@ class Dbus < Formula
     regex(/href=.*?dbus[._-]v?(\d+\.\d*?[02468](?:\.\d+)*)\.t/i)
   end
 
-  depends_on "autoconf" => :build
-  depends_on "autoconf-archive" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
+  head do
+    url "https://gitlab.freedesktop.org/dbus/dbus.git"  
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+
   depends_on "xmlto" => :build
 
   uses_from_macos "expat"
+
+  # Patch applies the config templating fixed in https://bugs.freedesktop.org/show_bug.cgi?id=94494
+  # Homebrew pr/issue: 50219
+  patch do
+    on_macos do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/0a8a55872e/d-bus/org.freedesktop.dbus-session.plist.osx.diff"
+      sha256 "a8aa6fe3f2d8f873ad3f683013491f5362d551bf5d4c3b469f1efbc5459a20dc"
+    end
+  end
 
   def install
     # Fix the TMPDIR to one D-Bus doesn't reject due to odd symbols
     ENV["TMPDIR"] = "/tmp"
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
 
-    args = [
-      "--disable-dependency-tracking",
-      "--prefix=#{prefix}",
-      "--localstatedir=#{var}",
-      "--sysconfdir=#{etc}",
-      "--enable-xml-docs",
-      "--disable-doxygen-docs",
-      "--without-x",
-      "--disable-tests",
-      "--disable-launchd",
-      "--with-dbus-user=root",
-      "--with-system-pid-file=/var/run/dbus.pid",
+    args = %w[
+      -Ddbus_user=root
+      -Dlaunchd=disabled
+      -Ddoxygen_docs=disabled
+      -Dlaunchd=disabled
+      -Dxml_docs=enabled
+      -Dx11_autolaunch=disabled
+      -Dmodular_tests=disabled
+      -Dsystem_pid_file=/var/run/dbus.pid
     ]
-    
-    system "./autogen.sh"
-    system "./configure", *args
-    system "make", "install"
-    system "rm", "-f", "#{etc}/dbus-1/session.conf"
-    system "rm", "-f", "#{etc}/dbus-1/system.conf"
+
+    system "meson", "setup", "build", *args, *std_meson_args, "--localstatedir=#{var}", "--sysconfdir=#{etc}"
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+    # system "rm", "-f", "#{etc}/dbus-1/session.conf"
+    # system "rm", "-f", "#{etc}/dbus-1/system.conf"
     system "mkdir", "-p", "#{etc}/dbus-1/system.d"
   end
 
@@ -56,10 +65,10 @@ class Dbus < Formula
 
   def post_install
     # Generate D-Bus's UUID for this machine
-    system "#{bin}/dbus-uuidgen", "--ensure=#{var}/lib/dbus/machine-id"
+    system bin/"dbus-uuidgen", "--ensure=#{var}/lib/dbus/machine-id"
   end
 
   test do
-    system "#{bin}/dbus-daemon", "--version"
+    system bin/"dbus-daemon", "--version"
   end
 end
